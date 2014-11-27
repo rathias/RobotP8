4753e6894e5ce8f3efd1691d49a9628a35fea47f
rathias <rathias@rathias-laptop>
2014-11-27 13:13:57 +0100

    change agentattaquant

--- RoboNewbie_1.0/src/gaming/AgentAttaquant.java
+++ RoboNewbie_1.0/src/gaming/AgentAttaquant.java
@@ -12,13 +12,31 @@ package gaming;
 public class AgentAttaquant extends Agent_SimpleWalkToBall {
       
     public static void main(String args[]) {
-    
-    String _team = args[0];
-    String _id = args[1];
-    double _beamX = Double.parseDouble(args[2]);
-    double _beamY = Double.parseDouble(args[3]);
-    double _beamRot = -90;
-    
+
+    String _team = "Testteam";
+    String _id = "test";
+      double _beamX = 0;
+      double _beamY = 0;
+      double _beamRot = -90;
+
+      if(args.length > 0)
+      {
+         _team = args[0];
+         _id = args[1];
+         _beamX = Double.parseDouble(args[2]);
+         _beamY = Double.parseDouble(args[3]);
+         _beamRot = -90;
+      }
+
+      if(args.length > 0)
+      {
+         _team = args[0];
+        _id = args[1];
+        _beamX = Double.parseDouble(args[2]);
+        _beamY = Double.parseDouble(args[3]);
+        _beamRot = -90;
+      }
+      
     Agent_SimpleWalkToBall agent = new Agent_SimpleWalkToBall();
     
     agent.init(_id, _team, _beamX, _beamY, _beamRot); 
